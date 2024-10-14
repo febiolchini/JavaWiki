fc2e101b6b0da649dc04c2a5460587e3644a7475
febiolchini <biolchinifederico@gmail.com>
2024-10-14 22:42:15 +0200

    first modification of project, try a sum

--- javaProject01/src/javaProject01/Main.java
+++ javaProject01/src/javaProject01/Main.java
@@ -1,10 +1,12 @@
 package javaProject01;
 
 public class Main {
-
+	public int sum(int x, int y){
+		return x+y;
+	}
+	int result = sum(3, 4);
 	public static void main(String[] args) {
 		System.out.println("hello, world!");
-
 	}
 
 }
